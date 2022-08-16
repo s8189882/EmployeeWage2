2e858f23e7b7a5957a3aea9a2bbb2f54c3fc6c3b
s8189882 <s8189882@gmail.com>
2022-08-16 09:31:36 +0530

    uc1-attendance

--- Employee Wage 2/src/javadirect/EmpWage.java
+++ Employee Wage 2/src/javadirect/EmpWage.java
@@ -4,7 +4,13 @@ public class EmpWage {
 	
 	public static void main(String args[]) {
 		
-		System.out.println("Welcome to Employee Wage Computation Program!");
+		int IS_FULL_TIME = 1;
+		
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 
 }
