b9ecb734f9d512ab199f590dd6bc4b2eeffa08c6
s8189882 <s8189882@gmail.com>
2022-08-16 13:55:51 +0530

    Calculating Wages for a Month assuming 20 Working Days in a Month

--- Employee Wage 2/src/javadirect/EmpWage.java
+++ Employee Wage 2/src/javadirect/EmpWage.java
@@ -5,41 +5,43 @@ public class EmpWage {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
 	
 	public static void main(String args[]) {
 		
 		int empHrs = 0;
 		int empWage = 0;
+		int totalEmpWage = 0;
 		
-		
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-		if (empCheck == IS_FULL_TIME)
-			empHrs = 8;
-		else
-			if (empCheck == IS_PART_TIME)
-				empHrs = 4;
-			else
-				empHrs = 0;
-		
-		switch (empCheck) {
-			case IS_FULL_TIME :
-				empHrs = 8;
-				System.out.println("Employee is present Full time.");
-				break;
-				
-			case IS_PART_TIME :
-				empHrs = 4;
-				System.out.println("Employee is present Part time.");
-				break;
-				
-			default :
-				empHrs = 0;
-				System.out.println("Employee is absent.");
-		
+		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+			
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			
+			switch (empCheck) {
+				case IS_FULL_TIME :
+					empHrs = 8;
+					System.out.println("Employee is present Full time.");
+					break;
+					
+				case IS_PART_TIME :
+					empHrs = 4;
+					System.out.println("Employee is present Part time.");
+					break;
+					
+				default :
+					empHrs = 0;
+					System.out.println("Employee is absent.");
+			
+			}
+			
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			totalEmpWage += empWage;
+			System.out.println("Employee Daily Wage : $" + empWage);
 		}
 		
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Employee Daily Wage : $" + empWage);
+		System.out.println();
+		System.out.println("Employee Monthly Wage : $" + totalEmpWage);
+		
 	}
 
 }
