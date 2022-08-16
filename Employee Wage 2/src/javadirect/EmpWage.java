c895ad18cc6e7e72776c6817dfe62b4918733314
s8189882 <s8189882@gmail.com>
2022-08-16 13:43:16 +0530

    Solving using Switch case

--- Employee Wage 2/src/javadirect/EmpWage.java
+++ Employee Wage 2/src/javadirect/EmpWage.java
@@ -2,16 +2,17 @@ package javadirect;
 
 public class EmpWage {
 	
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	
 	public static void main(String args[]) {
 		
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
 		int empHrs = 0;
 		int empWage = 0;
 		
 		
-		double empCheck = Math.floor(Math.random() * 10) % 3;
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 		if (empCheck == IS_FULL_TIME)
 			empHrs = 8;
 		else
@@ -19,6 +20,23 @@ public class EmpWage {
 				empHrs = 4;
 			else
 				empHrs = 0;
+		
+		switch (empCheck) {
+			case IS_FULL_TIME :
+				empHrs = 8;
+				System.out.println("Employee is present Full time.");
+				break;
+				
+			case IS_PART_TIME :
+				empHrs = 4;
+				System.out.println("Employee is present Part time.");
+				break;
+				
+			default :
+				empHrs = 0;
+				System.out.println("Employee is absent.");
+		
+		}
 		
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Employee Daily Wage : $" + empWage);
